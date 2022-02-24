b90d1635965847f2ca0ef810841c60f215113f22
Pedro <pedroaraujo_41@hotmail.com>
2022-02-24 21:29:41 +0000

    bug fix

--- app/src/main/java/com/example/testing/MainActivity.kt
+++ app/src/main/java/com/example/testing/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         println("Hello from Kotlin code!!")
         //TODO: something
+
+        //FIXME: corrigir bug
     }
 }
